e09b95f7c483890896be6d5973c375ab69742459
bspatafora <benjamin.spatafora@gmail.com>
2017-03-07 01:04:48 +0000

    Add SMSReceiver.Router.SMSReceived log

--- apps/sms_receiver/lib/sms_receiver/router.ex
+++ apps/sms_receiver/lib/sms_receiver/router.ex
@@ -1,15 +1,28 @@
 defmodule SMSReceiver.Router do
   use Plug.Router
+  require Logger
 
   plug :match
   plug :dispatch
 
   get "/receive" do
     conn = fetch_query_params(conn, [])
-    text = conn.query_params["text"]
+    text = param(conn, "text")
 
-    if text, do: SMSSender.send(text)
+    Logger.info("SMS received", [
+      name: "SMSReceiver.Router.SMSReceived",
+      smsID: param(conn, "messageId"),
+      smsRecipient: param(conn, "to"),
+      smsSender: param(conn, "msisdn"),
+      smsText: text,
+      smsTime: param(conn, "message-timestamp")])
+
+    SMSSender.send(text)
 
     send_resp(conn, 200, "")
+  end
+
+  defp param(conn, key) do
+    conn.query_params[key]
   end
 end
