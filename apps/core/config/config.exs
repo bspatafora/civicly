b17aa58d28221326cbfa9e30fc24c18ba541a1e3
bspatafora <benjamin.spatafora@gmail.com>
2017-04-11 21:03:59 -0500

    Specify absence of Core and SMSReceiver repos

--- apps/core/config/config.exs
+++ apps/core/config/config.exs
@@ -1,4 +1,6 @@
 use Mix.Config
 
+config :core, ecto_repos: []
+
 config :core, :sms_sender, SMSSender
 config :core, :storage_service, Storage.Service
--- apps/sms_receiver/config/config.exs
+++ apps/sms_receiver/config/config.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
+config :sms_receiver, ecto_repos: []
+
 config :sms_receiver, :port, 9001
