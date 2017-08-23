8cc7f097b55bdf00895be2b537f1ca863789e3cd
bspatafora <benjamin.spatafora@gmail.com>
2017-08-23 21:45:12 +0000

    Add input validation to new_iteration task

--- apps/iteration/lib/mix/tasks/new_iteration.ex
+++ apps/iteration/lib/mix/tasks/new_iteration.ex
@@ -10,9 +10,29 @@ defmodule Mix.Tasks.NewIteration do
     Application.ensure_all_started(:storage)
     Application.ensure_all_started(:sms_sender)
 
-    question = List.first(args)
-    year = List.last(args)
+    question = Enum.at(args, 0)
+    year = Enum.at(args, 1)
 
+    cond do
+      question == nil ->
+        output("You forgot to include the question")
+      !String.ends_with?(question, "?") ->
+        output("That question doesn't look right. Where's the question mark?")
+      year == nil ->
+        output("You forgot to include the year")
+      !String.match?(year, ~r/^\d{4}$/) ->
+        output("That year doesn't look right")
+      true ->
+        start_iteration(question, year)
+        output("Iteration #{year} has begun")
+    end
+  end
+
+  defp output(text) do
+    IO.puts(text)
+  end
+
+  defp start_iteration(question, year) do
     Assigner.group_by_twos()
     Notifier.notify(question, year)
   end
