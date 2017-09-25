6faeba4984aadab1c32a4883a8664fd09429851c
bspatafora <benjamin.spatafora@gmail.com>
2017-09-25 11:34:29 -0500

    Adjust SMSSender rate limit

--- apps/sms_sender/config/config.exs
+++ apps/sms_sender/config/config.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :sms_sender, :port, "9001"
 config :sms_sender, :path, "/send"
-config :sms_sender, :rate_limit, 2000
+config :sms_sender, :rate_limit, 3000
 
 import_config "#{Mix.env}.exs"
