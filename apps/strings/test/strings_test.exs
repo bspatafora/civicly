f944f9e5db01833cb8ac6e08ca22b579d2482d02
bspatafora <benjamin.spatafora@gmail.com>
2017-11-13 18:03:21 -0600

    Add missing Strings tests

--- apps/strings/test/strings_test.exs
+++ apps/strings/test/strings_test.exs
@@ -7,19 +7,25 @@ defmodule StringsTest do
   def messages do
     name = String.duplicate("a", 15)
     question = String.duplicate("a", 65)
+    title = "After victory in Raqqa over IS, Kurds face tricky peace"
+    url = "https://goo.gl/fbsS"
 
     iteration_start = S.iteration_start([name])
     question = S.question(question)
     partner_deletion = S.partner_deletion(name)
+    news = S.news(title, url)
 
     strings =
       [iteration_start,
        question,
+       S.conversation_reminder,
+       S.time_warning,
        S.iteration_end,
        S.empty_room,
        S.help,
        S.user_deletion,
-       partner_deletion]
+       partner_deletion,
+       news]
 
     step_1_part_1 = T.step_1_part_1(name)
     step_3_part_2 = T.step_3_part_2(name)
@@ -27,20 +33,16 @@ defmodule StringsTest do
     tutorial_strings =
       [step_1_part_1,
        T.step_1_part_2,
-       T.step_1_key,
        T.step_1_error,
        T.step_2_part_1,
        T.step_2_part_2,
-       T.step_2_key,
        T.step_2_error,
        T.step_3_part_1,
        step_3_part_2,
        T.step_4_part_1,
        T.step_4_part_2,
-       T.step_4_key,
        T.step_4_error,
        T.step_5,
-       T.step_5_key,
        T.step_5_error,
        T.complete_part_1,
        T.complete_part_2]
